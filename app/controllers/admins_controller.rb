b87f92411c97fc2e8590515f3f8e2c9bbfb82c29
almedaria <rmmgalmeda@gmail.com>
2021-12-08 02:17:54 +0800

    added set_admin in controller before action

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,16 @@
 class AdminsController < ApplicationController
+  before_action :set_admin, only: [:edit, :update, :destroy, :show]
   def index
     @admins = Admin.all
+  end
+
+  private
+
+  def set_admin
+      @admin = Admin.find(params[:id])
+  end
+  
+  def admin_params
+      params.require(:admin).permit(:email, :username, :fullname, :password)
   end
 end
