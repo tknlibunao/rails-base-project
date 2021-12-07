cd82ea548ccb155594f290a8e240a80b9bb4db2a
almedaria <rmmgalmeda@gmail.com>
2021-12-08 01:52:12 +0800

    added admins controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+  resources :admins
 end
