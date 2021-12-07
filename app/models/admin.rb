4eb02700b5262b4a06fc2fbaddf9a6120480658b
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-07 20:40:03 +0800

    reset repo, deleted admin and user CRUD

--- app/models/admin.rb
+++
@@ -1,6 +0,0 @@
-class Admin < ApplicationRecord
-    validates :username, :email, uniqueness: true, presence: true
-    validates :fullname, presence: true
-    validates :password, length: { minimum: 6 , message: "minimum 6 characters"}
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_040534) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "fullname"
-    t.string "password"
-
-ActiveRecord::Schema.define(version: 2021_12_04_031050) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "fullname"
-    t.string "email"
-    t.string "type"
-    t.boolean "verified", default: false
-    t.datetime "verified_at"
-
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+ActiveRecord::Schema.define(version: 0) do
 end
