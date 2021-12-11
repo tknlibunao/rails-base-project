1ee907b8e7f10e13aecc2d5ec75bdcb2d6f91cec
almedaria <rmmgalmeda@gmail.com>
2021-12-12 00:24:00 +0800

    added admins form details presence true

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,6 @@
 class Admin < ApplicationRecord
+    validates :first_name, :last_name, :contact_number, :address, presence: true
+    validates_format_of :contact_number,
+    :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+    :error => "- Phone numbers must be in xxx-xxx-xxxx format."
 end
