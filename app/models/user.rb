4255da406ab543fa1a11d989fd515ee41c1cd1b2
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-11 13:16:06 +0800

    removed fullname validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :fullname, presence: true
   validates :username, presence: true
 
 end
