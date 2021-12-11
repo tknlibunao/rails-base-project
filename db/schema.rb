3e4126b39d9615951acab55bd9acc488671a47ef
almedaria <rmmgalmeda@gmail.com>
2021-12-12 02:37:46 +0800

    added controllers and models for wallets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_164214) do
+ActiveRecord::Schema.define(version: 2021_12_11_182434) do
 
   create_table "admins", force: :cascade do |t|
     t.string "first_name"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2021_12_11_164214) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.integer "wallet_id"
+    t.decimal "actual_balance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
