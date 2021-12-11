874f41d5d153140d4d77034150d4b01d85a8b863
almedaria <rmmgalmeda@gmail.com>
2021-12-11 23:18:01 +0800

    removed unused database and added new admins models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_104631) do
+ActiveRecord::Schema.define(version: 2021_12_11_145143) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "contact_number"
+    t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
