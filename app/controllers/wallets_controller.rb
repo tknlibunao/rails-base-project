105ce17f912bb3ac53aec0c9fce595fe1805a2f4
almedaria <rmmgalmeda@gmail.com>
2021-12-12 02:48:38 +0800

    updated wallets controller

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,56 @@
 class WalletsController < ApplicationController
+    before_action :set_wallet, only: [:edit, :update, :destroy, :show]
+
+    def index
+        @wallets = Wallet.all
+    end
+
+    def new
+        @wallet = Wallet.new
+    end
+
+    def show
+        @wallet = Wallet.find(params[:id])
+        
+    end
+
+    def create
+        @wallet = Wallet.new(wallet_params)
+
+        if @wallet.save
+            flash[:notice] = "A new wallet has been added"
+            redirect_to wallets_path
+        else
+            flash[:error] = "There are some errors encountered"
+            render :new
+        end
+    end
+
+    def edit
+        @wallet = wallet.find(params[:id])
+    end
+
+    def update
+
+        if @wallet.update(wallet_params)
+            redirect_to wallets_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @wallet.destroy
+        redirect_to wallets_path, notice: "The wallet was successfully deleted"
+    end
+
+    private
+
+    def set_wallet
+        @wallet = wallet.find(params[:id])
+    end
+
+    def wallet_params
+        params.require(:wallet).permit(:buyer_id, :actual_balance)
+    end
 end
