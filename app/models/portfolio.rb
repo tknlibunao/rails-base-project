dc8e257953c8a4e5809ef6f07c1b30572a0635c9
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-13 18:20:03 +0800

    modified portfolio model, included has_many_through association

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,8 @@
 class Portfolio < ApplicationRecord
   validates :buyer_id, presence: true
+
+  has_many :market_portfolios
+  has_many :markets, through: :market_portfolios
+
+  accepts_nested_attributes_for :market_portfolios
 end
