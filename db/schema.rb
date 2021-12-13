54cb56c10df8b90968272d07ea16fbc44e505be8
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-13 15:05:37 +0800

    migrated portfolios db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_104631) do
+ActiveRecord::Schema.define(version: 2021_12_13_053926) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.integer "buyer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
