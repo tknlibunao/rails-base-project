0860ac47d52422e4f10fb75038de3738515a37b1
almedaria <rmmgalmeda@gmail.com>
2021-12-15 05:26:34 +0800

    updated syntax and added routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, presence: true
 
-  has_one :admin, :buyer
+  has_one :admin
+  has_one :buyer
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   resources :users, :admins, :buyers, :wallets, :portfolios, :markets
 
   root to: "home#index"
+
+  resources :admins do
+    resources :buyers
+  end
+
 end
