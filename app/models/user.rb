00879327d9aaa7e25651e5a315a4e30e90b81304
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-28 23:31:37 +0800

    added rolify, and admin? and buyer? methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,5 +8,13 @@ class User < ApplicationRecord
 
   has_one :admin
   has_one :buyer
+    
+  def admin?
+    has_role?(:admin)
+  end
+
+  def buyer?
+    has_role?(:buyer)
+  end
 
 end
