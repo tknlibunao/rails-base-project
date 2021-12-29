e2f454c65e877ff2f6500fbdc492c92806dcfd84
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-29 13:15:50 +0800

    changed buyer_id into account_id as FK

--- db/migrate/20211213053926_create_portfolios.rb
+++ db/migrate/20211213053926_create_portfolios.rb
@@ -1,7 +1,7 @@
 class CreatePortfolios < ActiveRecord::Migration[6.0]
   def change
     create_table :portfolios do |t|
-      t.integer :buyer_id, foreign_key: true
+      t.integer :account_id, foreign_key: true
 
       t.timestamps
     end
