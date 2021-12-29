49b9f6a2ad010823662a3630c2b73393a5a1872d
almedaria <rmmgalmeda@gmail.com>
2021-12-30 03:55:21 +0800

    updated to accounts_path - still needs change in if else

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.user_type == 'Admin'
-      admins_path
+      accounts_path
     else
-      buyers_path
+      accounts_path
     end
   end
 end
