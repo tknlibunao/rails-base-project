eeadbf51314204e3b13a6526197f14d91bd26f0c
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-29 13:27:02 +0800

    migrated db for accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_080313) do
+ActiveRecord::Schema.define(version: 2021_12_29_015734) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "contact_number"
+    t.text "address"
+    t.boolean "verified", default: false
+    t.datetime "verified_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "first_name"
