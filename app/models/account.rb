a0811b3ffc7d0f74398ddadf2d6462218ecdad34
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-12-29 13:05:40 +0800

    added Account model, with validation and relationship to User, Wallet, Portfolio

---
+++ app/models/account.rb
@@ -0,0 +1,9 @@
+class Account < ApplicationRecord
+  validates :first_name, :last_name, :contact_number, :address, :verified, :verified_at, presence: true
+  validates :verified, default: false
+  
+  belongs_to :user
+  
+  has_one :wallet
+  has_one :portfolio
+end
