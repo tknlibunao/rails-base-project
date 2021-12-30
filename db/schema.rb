ce0e3ea67d80c5f7b59c32e182dd6db090f2da33
almedaria <rmmgalmeda@gmail.com>
2021-12-31 01:42:13 +0800

    migrated additional account_id FK column for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_193128) do
+ActiveRecord::Schema.define(version: 2021_12_30_173716) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_12_29_193128) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "user_type"
+    t.integer "account_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
