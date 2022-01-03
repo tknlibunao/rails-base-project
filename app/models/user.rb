8e068bf0492c06e338acaa2b2abace876224b11e
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-03 16:59:53 +0800

    added User after_create callback for newly added methods :set_role and :create_account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   validates :username, :user_type, presence: true
 
   has_one :account
+  
+  after_create :set_role, :create_account
     
   def admin?
     has_role?(:admin)
@@ -14,6 +16,22 @@ class User < ApplicationRecord
 
   def buyer?
     has_role?(:buyer)
+  end
+
+  def create_account
+    if self.user_type == 'Admin'
+      Account.create(user: self, first_name: 'Sample', last_name: 'Account', verified: true)
+    elsif self.user_type == 'Buyer'
+      Account.create(user: self, first_name: 'Sample', last_name: 'Account')
+    end
+  end
+
+  def set_role
+    if self.user_type == 'Admin'
+      self.add_role(:admin)
+    elsif self.user_type == 'Buyer'
+      self.add_role(:buyer)
+    end
   end
 
 end
