57d7668d2ef5d267a95b68ceaa687fbf1b2be5dc
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-03 18:03:40 +0800

    added new method set_user, modified create method to relate User and Account instances

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,20 +8,22 @@ class AccountsController < ApplicationController
     @accounts = Account.all
   end
 
-  def new
-    @account = Account.new
-  end
+  # def new
+  #   @account = Account.new
+  # end
 
   def show
   end
 
   def create
-    @account = Account.new(account_params)
+    # @account = Account.new(account_params)
+   
+    @account = @user.account.build(account_params)
     
     if @account.save
-      redirect_to accounts_path, notice: 'An account was successfully created'
+      redirect_to account_path(@account), notice: 'An account was successfully created'
     else
-      render :new, error: 'Account cannot be created due to some errors'
+      render user_session_path, error: 'Account cannot be created due to some errors'
     end
   end
 
@@ -30,7 +32,7 @@ class AccountsController < ApplicationController
 
   def update
     if @account.update(account_params)
-      redirect_to accounts_path, notice: 'Account successfully updated'
+      redirect_to account_path(@account), notice: 'Account successfully updated'
     else
       render :edit, error: 'Account cannot be updated due to some errors'
     end
@@ -44,12 +46,16 @@ class AccountsController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+  
   def set_account
     begin
       @account = Account.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-      redirect_to new_account_path
-      p 'Redirected to new portfolio'
+      redirect_to user_session_path
+      p 'Redirected to sign in'
     end
   end
 
