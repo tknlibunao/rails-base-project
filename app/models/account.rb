e192814a04416b31787b9e104181f882e41dc2c0
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-06 20:11:34 +0800

    added create_wallet method and used callback after_createin Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,4 +7,11 @@ class Account < ApplicationRecord
   has_one :wallet
   has_one :portfolio
   
+  after_create :create_wallet
+
+  def create_wallet
+    if self.user.user_type == 'Buyer'
+      Wallet.create(account: self, actual_balance: 50000)
+    end
+  end
 end
