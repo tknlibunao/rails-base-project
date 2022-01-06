1d17d3e7935b2dd9f3d45799cd1ed2b9218e57b5
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-06 20:03:58 +0800

    added begin-rescue ActiveRecord::RecordNotFound to set_wallet method

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -8,36 +8,37 @@ class WalletsController < ApplicationController
         @wallets = Wallet.all
     end
 
-    def new
-        @wallet = Wallet.new
-    end
+    # def new
+    #     @wallet = Wallet.new
+    # end
 
     def show
-        @wallet = Wallet.find(params[:id])
+        #@wallet = Wallet.find(params[:id])
         
     end
 
     def create
-        @wallet = Wallet.new(wallet_params)
-        #@wallet.user_id = current_user.id
+        # @wallet = Wallet.new(wallet_params)
+        
+        @wallet = @account.wallet.build(wallet_params)  
 
         if @wallet.save
             flash[:notice] = "A new wallet has been added"
-            redirect_to wallets_path
+            redirect_to wallet_path(@wallet)
         else
             flash[:error] = "There are some errors encountered"
-            render :new
+            render account_path(@account)
         end
     end
 
     def edit
-        @wallet = Wallet.find(params[:id])
+        # @wallet = Wallet.find(params[:id])
     end
 
     def update
 
         if @wallet.update(wallet_params)
-            redirect_to wallets_path
+            redirect_to wallet_path(@wallet)
         else
             render :edit
         end
@@ -50,11 +51,20 @@ class WalletsController < ApplicationController
 
     private
 
+    def set_account
+      @account = Account.find(params[:account_id])
+    end
+
     def set_wallet
-        @wallet = Wallet.find(params[:id])
+        begin
+            @wallet = Wallet.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            redirect_to root_url
+            p 'Redirected to home'
+        end
     end
 
     def wallet_params
-        params.require(:wallet).permit(:buyer_id, :actual_balance)
+        params.require(:wallet).permit(:actual_balance)
     end
 end
