b0f8f576766c7c6cad33c3e8bab1156500a0cdbd
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-07 16:59:14 +0800

    added new create_portfolio method to after_create callback in Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,11 +7,17 @@ class Account < ApplicationRecord
   has_one :wallet
   has_one :portfolio
   
-  after_create :create_wallet
+  after_create :create_wallet, :create_portfolio
 
   def create_wallet
     if self.user.user_type == 'Buyer'
       Wallet.create(account: self, actual_balance: 50000)
+    end
+  end
+
+  def create_portfolio
+    if self.user.user_type == 'Buyer'
+      Portfolio.create(account: self)
     end
   end
 end
