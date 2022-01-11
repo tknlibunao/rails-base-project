8604ae6ded9c4bcadc074f1dacfbe6adb29cd63b
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-11 10:17:15 +0800

    added has_many_through relationship with Transaction, commented out HMT relationship with MarketPortfolio

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,6 +1,9 @@
 class Market < ApplicationRecord
   validates :stock_name, :volume, presence: true
 
-  has_many :market_portfolios
-  has_many :porfolios, through: :market_portfolios
+  # has_many :market_portfolios
+  # has_many :porfolios, through: :market_portfolios
+  
+  has_many :transactions
+  has_many :porfolios, through: :transactions
 end
