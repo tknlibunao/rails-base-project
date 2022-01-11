c97ff0b2ee3ed1846d7a406e20c32bf2cc1d50da
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-11 19:36:21 +0800

    modified market_portfolios controller (testing for WIP buy and sell)

--- app/controllers/market_portfolios_controller.rb
+++ app/controllers/market_portfolios_controller.rb
@@ -1,17 +1,21 @@
 class MarketPortfoliosController < ApplicationController
-  before_action :set_market, :set_portfolio
+  # before_action :set_market, :set_portfolio
   
   def new
+    @market = Market.find(params[:market_id])
+    @portfolio = current_user.account.portfolio
+    @wallet = current_user.account.wallet
     @market_portfolio = MarketPortfolio.new
   end
 
   def create
-    # @market_portfolio = MarketPortfolio.new(market_portfolio_params)
+    # @market = Market.find(params[:market_id])
+    @market_portfolio = MarketPortfolio.new(market_portfolio_params)
 
     # @market_portfolio.portfolio = current_user.account.portfolio
     # @market_portfolio.market = market
     
-    @market_portfolio = @portfolio.market_portfolio.build(market_portfolio_params)
+    # @market_portfolio = @portfolio.market_portfolio.build(market_portfolio_params)
 
     if @market_portfolio.save
       redirect_to markets_path, notice: "Transaction successful!"
@@ -23,7 +27,7 @@ class MarketPortfoliosController < ApplicationController
   private
 
   def set_market
-    @market = Market.find(params[:market_id])
+    @market = Market.find(params[:id])
   end
 
   def set_portfolio
@@ -36,6 +40,6 @@ class MarketPortfoliosController < ApplicationController
   end
 
   def market_portfolio_params
-    params.require(:market_portfolio).permit(:price_bought, :volume_bought)  
+    params.require(:market_portfolio).permit(:price_bought, :volume_bought, :price_sold, :volume_sold, :transaction_type, :market_id, :portfolio_id)  
   end
 end
