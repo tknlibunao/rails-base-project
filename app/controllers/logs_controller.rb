2c93200d35824d30a6a501b4f5d192c6f77a73b7
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-11 19:31:09 +0800

    added logs controller (WIP, only for new and create)

---
+++ app/controllers/logs_controller.rb
@@ -0,0 +1,32 @@
+class LogsController < ApplicationController
+  before_action :set_portfolio
+  
+  def new
+    @log = Log.new
+    @market = Market.find(params[:market_id])
+    @wallet = current_user.account.wallet
+  end
+
+  def create
+    @log = Log.new(log_params)
+    # @log = @portfolio.log.build(log_params)
+
+    if @log.save
+      redirect_to portfolio_path(@portfolio), notice: "Transaction successful"
+    else
+      render :new, error: "Transaction failed"
+    end
+
+  end
+
+  private
+
+  def set_portfolio
+    @portfolio = current_user.account.portfolio
+  end
+
+  def log_params
+    params.require(:log).permit(:price_bought, :volume_bought, :price_sold, :volume_sold, :kind, :market_id, :portfolio_id)
+  end
+
+end
