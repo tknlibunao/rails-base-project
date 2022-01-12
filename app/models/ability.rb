199a4e7a48751d189c36f27fe7352b4cc0dda218
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-12 16:43:26 +0800

    modified ability.rb such that only verified users can access the index pages of each model (not working for markets and logs though)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    if user.buyer?
-      can :manage, Market
+    if user.buyer? && user.account.verified
       can :read, Wallet, {:account_id => user.account.id}
       can :manage, Portfolio, {:account_id => user.account.id}
       can [:read, :update], Account, {:user_id => user.id, :verified => true}
