9617d67b751f640d32f288cb439d97134dad1a5d
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-13 03:43:21 +0800

    added routes (via scope) for the endpoints (films, people, locations)

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,23 @@ Rails.application.routes.draw do
     get '/latest/exchangerate', to: 'exchangerates#index'
  end
 
+  # namespace :api do
+  #   get '/ghibli/films', to: 'studio_ghiblis#index'
+  # end
+
   scope module: 'api', path: 'separate_path', as: 'different_prefix'  do
     get '/today/quote', to: 'quotes#index'
+  end
+
+  scope module: 'api', path: 'anime', as: 'anime' do
+    get '/ghibli/films', to: 'studio_ghiblis#films'
+  end
+  
+  scope module: 'api', path: 'anime', as: 'anime' do
+    get '/ghibli/people', to: 'studio_ghiblis#people'
+  end
+  
+  scope module: 'api', path: 'anime', as: 'anime' do
+    get '/ghibli/locations', to: 'studio_ghiblis#locations'
   end
 end
