9c899b99045bb0301fd0d4a0bbcea2f0f6d95251
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-01-13 16:53:38 +0800

    added sell logic in the check_transaction method (still for testing)

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -43,25 +43,52 @@ class LogsController < ApplicationController
       
       # proceed with transaction if wallet balance > 0 after
       if @wallet.actual_balance.positive?
-        if @log.save
+        if log.save
           if @wallet.save
             redirect_to portfolios_path, notice: "Transaction successful"
           else
             redirect_back fallback_location: portfolios_path, alert: "Error in updating wallet"
           end
-
         else
           redirect_back fallback_location: portfolios_path, alert: "Transaction failed"
         end
-        
       else
         redirect_back fallback_location: portfolios_path, alert: "Please check your wallet balance"
       end
-
     when "Sell"
-      
+      # check if stock units >= volume_sold
+      remaining_units = compute_stock_units(log) - log.volume_sold
+
+      # proceed with transaction if stock units > 0 after
+      if remaining_units.positive?
+        if log.save
+          @wallet.balance = current_user.account.wallet.actual_balance + compute_stock_revenue(log)
+          if @wallet.save
+            redirect_to portfolios_path, notice: "Transaction successful"
+          else
+            redirect_back fallback_location: portfolios_path, alert: "Error in updating wallet"
+          end
+        else
+          redirect_back fallback_location: portfolios_path, alert: "Transaction failed"
+        end
+      else
+        redirect_back fallback_location: portfolios_path, alert: "Please check your owned stocks"
+      end
     else
     end
+  end
+
+  def compute_stock_revenue(log)
+    log.volume_sold * log.market.selling_price
+  end
+
+  def compute_stock_units(log)
+    @stocks = Log.where(portfolio_id: @portfolio.id, market_id: log.market_id)
+    
+    units_bought = @stocks.sum(:price_bought)/log.market.buying_price
+    units_sold = @stocks.sum(:volume_sold)
+
+    units_bought - units_sold
   end
 
   def log_params
